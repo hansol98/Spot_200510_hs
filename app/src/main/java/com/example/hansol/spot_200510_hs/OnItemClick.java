35c4268d4f5253c7c9385af9edec4b219d376783
hansol <ehs1002@naver.com>
2020-05-16 12:16:05 +0900

    0516
    0516(지연언니꺼 합치기 전!)

---
+++ app/src/main/java/com/example/hansol/spot_200510_hs/OnItemClick.java
@@ -0,0 +1,7 @@
+package com.example.hansol.spot_200510_hs;
+
+public interface OnItemClick {
+    void onClick(double x, double y, String name);
+    void make_db(String countId, String name);
+    void make_dialog();
+}
